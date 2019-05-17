62c64c2ec80f390ab8c9c97d9682e4dfb1c5f49f
Aaron Wright <aaron.c.wright@icloud.com>
2019-05-17 07:44:44 -0500

    Initial Commit

---
+++ Licensed/Licensed.h
@@ -0,0 +1,19 @@
+//
+//  Licensed.h
+//  Licensed
+//
+//  Created by Aaron Wright on 5/17/19.
+//  Copyright © 2019 Infinite Token. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Licensed.
+FOUNDATION_EXPORT double LicensedVersionNumber;
+
+//! Project version string for Licensed.
+FOUNDATION_EXPORT const unsigned char LicensedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Licensed/PublicHeader.h>
+
+
